5ab7a9c6355089d71ecabb69342cc843e6038afa
Infina Mac 6 <jppm8387@yahoo.com>
2015-12-30 15:49:20 -0500

    Chapter 5 oop

--- javascript/The Principles of OOP/Chapter4.js
+++ javascript/The Principles of OOP/Chapter4.js
@@ -58,9 +58,173 @@ alert("hasOwnProperty" in book); // true
 alert(book.hasOwnProperty("hasOwnProperty")); // false
 alert(book.hasOwnProperty("title")); // true
 alert(Object.prototype.hasOwnProperty("hasOwnProperty")); // true
-
+//determine whether a property is on the prototype
 function hasPrototypeProperty(object, name) {
 	return name in object && !object.hasOwnProperty(name);
 }
 alert(hasPrototypeProperty(book, "title")); // false
 alert(hasPrototypeProperty(book, "hasOwnProperty")); // true
+
+var object = {};
+//Object.getPrototypeOf reads the value of the [[Prototype]] property
+var prototype = Object.getPrototypeOf(object);
+alert(prototype === Object.prototype);
+
+var object = {};
+//isPrototypeOf - to see if one object is a prototype for another
+alert(Object.prototype.isPrototypeOf(object)); // true
+
+var object = {};
+alert(object.toString()); // "[object Object]"
+object.toString = function() {
+	return "[object Custom]";
+};
+alert(object.toString()); // "[object Custom]"
+// delete own property
+delete object.toString;
+alert(object.toString()); // "[object Object]"
+
+// no effect on prototype - delete only works on own properties
+delete object.toString;
+console.log(object.toString()); // "[object Object]"
+
+/****************** Using Prototypes with Constructors **************************/
+function Person(name) {
+	this.name = name;
+}
+//careful when using reference values. Because these values are shared across instances,
+Person.prototype.sayName = function() {
+	alert(this.name);
+};
+var person1 = new Person("Nicholas");
+var person2 = new Person("Greg");
+alert(person1.name); // "Nicholas"
+alert(person2.name); // "Greg"
+person1.sayName(); // outputs "Nicholas"
+person2.sayName(); // outputs "Greg"
+
+
+function Person(name) {
+	this.name = name;
+}
+Person.prototype.sayName = function() {
+	alert(this.name);
+};
+//favorites is shared using prototype
+Person.prototype.favorites = [];
+var person1 = new Person("Nicholas");
+var person2 = new Person("Greg");
+person1.favorites.push("pizza");
+person2.favorites.push("quinoa");
+alert(person1.favorites); // "pizza,quinoa"
+alert(person2.favorites); // "pizza,quinoa"
+
+//Constructor
+function Person(name) {
+	this.name = name;
+}
+//To avoid overwriting prototype object , restore the constructor property
+Person.prototype = {
+  constructor: Person,
+  sayName: function() {
+  	alert(this.name);
+  },
+  toString: function() {
+  return "[Person " + this.name + "]";
+  }
+};
+//Instances
+var person1 = new Person("Nicholas");
+var person2 = new Person("Greg");
+alert(person1 instanceof Person); // true
+alert(person1.constructor === Person); // true
+alert(person1.constructor === Object); // false
+alert(person2 instanceof Person); // true
+alert(person2.constructor === Person); // true
+alert(person2.constructor === Object); // false
+/*
+See Page 60. For Image
+Note: disruption between the instance and the prototype will also create a disruption between the
+instance and the constructor.
+*/
+
+function Person(name) {
+	this.name = name;
+}
+Person.prototype = {
+  constructor: Person,
+  sayName: function() {
+  	alert(this.name);
+  },
+  toString: function() {
+  	return "[Person " + this.name + "]";
+  }
+};
+var person1 = new Person("Nicholas");
+var person2 = new Person("Greg");
+alert("sayHi" in person1); // false
+alert("sayHi" in person2); // false
+
+// add a new method (changes the prototype as well as the instances)
+Person.prototype.sayHi = function() {
+	alert("Hi");
+};
+person1.sayHi(); // outputs "Hi"
+person2.sayHi(); // outputs "Hi"
+var person1 = new Person("Nicholas");
+var person2 = new Person("Greg");
+Object.freeze(person1);
+Person.prototype.sayHi = function() {
+	alert("Hi");
+};
+person1.sayHi(); // outputs "Hi"
+person2.sayHi(); // outputs "Hi"
+
+
+
+function Person(name) {
+	this.name = name;
+}
+Person.prototype = {
+  constructor: Person,
+  sayName: function() {
+  	alert(this.name);
+  },
+  toString: function() {
+  	return "[Person " + this.name + "]";
+  }
+};
+var person1 = new Person("Nicholas");
+var person2 = new Person("Greg");
+Object.freeze(person1);
+//Even though person1 is freeze, you can still change the constructor prototype where it inhireted
+Person.prototype.sayHi = function() {
+	alert("Hi");
+};
+person1.sayHi(); // outputs "Hi"
+person2.sayHi(); // outputs "Hi"
+
+
+/****************** Built-in Object Prototypes **************************/
+
+//All built-in objects have constructors, and therefore, they have prototypes that you can change
+//adding a new method for use on all arrays is as simple as modifying Array.prototype
+Array.prototype.sum = function() {
+	return this.reduce(function(previous, current) {
+		return previous + current;
+	});
+};
+var numbers = [ 1, 2, 3, 4, 5, 6 ];
+var result = numbers.sum();
+alert(result); // 21
+
+
+String.prototype.capitalize = function() {
+	return this.charAt(0).toUpperCase() + this.substring(1);
+};
+var message = "hello world!";
+alert(message.capitalize()); // "Hello world!"
+
+
+
+
---
+++ javascript/The Principles of OOP/chapter5.js
@@ -0,0 +1,245 @@
+/****************** Prototype Chaining and Object.prototype **************************/
+// Inheritance
+//Prototype Chaining - An object inherits from its prototype, while that prototype in turn inherits from its prototype, and so on.
+//Object.prototype - object literal has its [[Prototype]] set to Object.prototype, meaning that it inherits properties from Object.prototype
+
+/*
+Methods Inherited from Object.prototype
+- hasOwnProperty() Determines whether an own property with the given name exists
+- propertyIsEnumerable() Determines whether an own property is enumerable
+- isPrototypeOf() Determines whether the object is the prototype of another
+- valueOf() Returns the value representation of the object
+- toString() Returns a string representation of the object
+*/
+
+var now = new Date();
+var earlier = new Date(2010, 1, 1);
+//valueOf() method gets called whenever an operator is used
+alert(now > earlier); // true
+
+
+var book = {
+	title: "The Principles of Object-Oriented JavaScript"
+};
+var message = "Book = " + book;
+alert(message); // "Book = [object Object]"
+
+
+var book = {
+	title: "The Principles of Object-Oriented JavaScript",
+	toString: function() {
+		return "[Book " + this.title + "]"
+	}
+};
+var message = "Book = " + book;
+// "Book = [Book The Principles of Object-Oriented JavaScript]"
+alert(message);
+
+/****************** Modifying Object.prototype **************************/
+Object.prototype.add = function(value) {
+	return this + value;
+};
+var book = {
+	title: "The Principles of Object-Oriented JavaScript"
+};
+alert(book.add(5)); // "[object Object]5"
+alert("title".add("end")); // "titleend"
+// in a web browser
+alert(document.add(true)); // "[object HTMLDocument]true"
+alert(window.add(5)); // "[object Window]true"
+
+//Object.prototype.add() is an enumerable property, which means it will show up when you use a for-in loop,
+Object.prototype.add = function(value) {
+	return this + value;
+};
+var empty = {};
+for (var property in empty) {
+ alert(property);
+}
+
+
+//hasOwnProperty - effective against possible unwanted prototype properties
+Object.prototype.add = function(value) {
+	return this + value;
+};
+var empty = {};
+for (var property in empty) {
+  if (empty.hasOwnProperty(property)) {
+    alert(property);
+  }
+}
+
+/****************** Object Inheritance **************************/
+//object literal
+var book = {
+	title: "The Principles of Object-Oriented JavaScript"
+};
+
+//Object.create() - explicitly specify [[Prototype]]
+var book = Object.create(Object.prototype, { //This book is the same as above
+  title: {
+  configurable: true,
+  enumerable: true,
+  value: "The Principles of Object-Oriented JavaScript",
+  writable: true
+  }
+});
+
+
+//person1 object inherits from Object.prototype
+var person1 = {
+	//property
+	name: "Nicholas",
+	//method
+	sayName: function() {
+		alert(this.name);
+	}
+};
+//person2 object inherits from person1
+var person2 = Object.create(person1, {
+  //name shadow property
+  name: {
+    configurable: true,
+    enumerable: true,
+    value: "Greg",
+    writable: true
+  }
+});
+person1.sayName(); // outputs "Nicholas"
+person2.sayName(); // outputs "Greg"
+alert(person1.hasOwnProperty("sayName")); // true
+alert(person1.isPrototypeOf(person2)); // true
+alert(person2.hasOwnProperty("sayName")); // false
+//see figure: Figure 5-1
+
+//object with no prototype chain
+var nakedObject = Object.create(null);
+alert("toString" in nakedObject); // false
+alert("valueOf" in nakedObject); // false
+
+/****************** Constructor Inheritance **************************/
+
+// you write this (Super Type)
+function YourConstructor() {
+	// initialization
+}
+// JavaScript engine does this for you behind the scenes (Sub Type)
+YourConstructor.prototype = Object.create(Object.prototype, {
+  constructor: {
+    configurable: true,
+    enumerable: true,
+    value: YourConstructor
+    writable: true
+  }
+});
+
+
+
+
+//constructors
+function Rectangle(length, width) {
+  this.length = length;
+  this.width = width;
+}
+Rectangle.prototype.getArea = function() {
+	return this.length * this.width;
+};
+Rectangle.prototype.toString = function() {
+	return "[Rectangle " + this.length + "x" + this.width + "]";
+};
+//constructors
+function Square(size) {
+	this.length = size;
+	this.width = size;
+}
+//Square.prototype inherits from Rectangle
+Square.prototype = new Rectangle();
+Square.prototype.constructor = Square;
+Square.prototype.toString = function() {
+	return "[Square " + this.length + "x" + this.width + "]";
+};
+var rect = new Rectangle(5, 10);
+var square = new Square(6);
+alert(rect.getArea()); // 50
+alert(square.getArea()); // 36
+alert(rect.toString()); // "[Rectangle 5x10]"
+alert(square.toString()); // "[Square 6x6]"
+alert(rect instanceof Rectangle); // true
+alert(rect instanceof Object); // true
+alert(square instanceof Square); // true
+alert(square instanceof Rectangle); // true
+alert(square instanceof Object); // true
+//see 5.2 page 74 for image
+
+
+
+//constructors
+function Rectangle(length, width) {
+  this.length = length;
+  this.width = width;
+}
+// inherits from Rectangle 
+function Square(size) {
+  this.length = size;
+  this.width = size;
+}
+//Rectangle.prototype overwrites Square.prototype
+Square.prototype = Object.create(Rectangle.prototype, {
+//overrides default Rectangle constructor within square
+  constructor: {
+    configurable: true,
+    enumerable: true,
+    value: Square,
+    writable: true
+  }
+});
+Square.prototype.toString = function() {
+	return "[Square " + this.length + "x" + this.width + "]";
+};
+
+/****************** Constructor Stealing **************************/
+
+function Rectangle(length, width) {
+	this.length = length;
+	this.width = width;
+}
+Rectangle.prototype.getArea = function() {
+	return this.length * this.width;
+};
+Rectangle.prototype.toString = function() {
+	return "[Rectangle " + this.length + "x" + this.width + "]";
+};
+// inherits from Rectangle
+function Square(size) {
+  Rectangle.call(this, size, size);
+  // optional: add new properties or override existing ones here
+}
+Square.prototype = Object.create(Rectangle.prototype, {
+  constructor: {
+    configurable: true,
+    enumerable: true,
+    value: Square,
+    writable: true
+  }
+});
+Square.prototype.toString = function() {
+	return "[Square " + this.length + "x" + this.width + "]";
+};
+var square = new Square(6);
+alert(square.length); // 6
+alert(square.width); // 6
+alert(square.getArea()); // 36
+//See page 76
+
+
+
+
+
+
+
+
+
+
+
+
+
