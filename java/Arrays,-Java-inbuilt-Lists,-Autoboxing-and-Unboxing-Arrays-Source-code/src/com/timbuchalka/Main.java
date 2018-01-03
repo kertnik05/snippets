d53c09cafe0b0268a94e91cd775f56c3536a7e0c
john <jmariano@infina.net>
2018-01-03 12:43:33 -0500

    Java

---
+++ java/Arrays,-Java-inbuilt-Lists,-Autoboxing-and-Unboxing-Arrays-Source-code/src/com/timbuchalka/Main.java
@@ -0,0 +1,49 @@
+package com.timbuchalka;
+
+import java.util.Scanner;
+
+public class Main {
+
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int[] myIntegers = getIntegers(5);
+        for(int i=0; i<myIntegers.length; i++) {
+            System.out.println("Element " + i +", typed value was " + myIntegers[i]);
+        }
+        System.out.println("The average is " + getAverage(myIntegers));
+        
+        /*
+        int[] intArray1 = new int[10];
+        intArray1[0] = 35;
+        intArray1[1] = 50;
+
+        int[] intArray2 = {1,2,3,4,5,6,7,8,9,10}
+
+        double[] doubleArray = new double[10];*/
+
+
+    }
+
+    public static int[] getIntegers(int number) {
+        System.out.println("Enter " + number + " integer values.\r");
+        int[] values = new int[number];
+
+        for(int i=0; i<values.length; i++) {
+            values[i] = scanner.nextInt();
+        }
+
+        return values;
+    }
+
+    public static double getAverage(int[] array) {
+        int sum = 0;
+        for(int i=0; i< array.length; i++) {
+            sum += array[i];
+        }
+
+        return (double) sum / (double)array.length;
+
+    }
+
+}
