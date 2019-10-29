32e66ec35688e1bcef99139dbd851b2d8c8e8576
johnpaulmariano <john.mariano.ctr@usuhs.edu>
2019-10-29 08:14:20 -0400

    React

--- javascript/react/redux--04-improved-setup/src/index.js
+++ javascript/react/redux--04-improved-setup/src/index.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux'; //5 
+import { createStore, combineReducers } from 'redux'; //1
 
-import counterReducer from './store/reducers/counter';
-import resultReducer from './store/reducers/result';
+import counterReducer from './store/reducers/counter'; //3
+import resultReducer from './store/reducers/result'; //3
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+//4
 const rootReducer = combineReducers({
     ctr: counterReducer,
     res: resultReducer
 });
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer); //2
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
